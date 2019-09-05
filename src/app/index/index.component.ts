6b9aba2543062b72d8b3831f6823cc0c39fed681
CookieRamen <ark@arkjp.net>
2019-09-05 16:01:22 +0900

    Add background particle.

--- src/app/index/index.component.ts
+++ src/app/index/index.component.ts
@@ -72,10 +72,16 @@ export class IndexComponent implements OnInit {
     '証明を探している'
   ];
 
+  particleStyle: object = {};
+  particleParams: object = {};
+  particleWidth = 100;
+  particleHeight = 100;
+
   // 実際に表示されるデータに代入するための変数
   bgText: string[] = [];
 
-  constructor() { }
+  constructor() {
+  }
 
   // フレームワークの準備が完了した際のトリガー
   ngOnInit() {
@@ -87,6 +93,58 @@ export class IndexComponent implements OnInit {
     for (let i = 0; i < 20; i++) {
       this.bgText.push(this.bgTextData[Math.floor(Math.random() * this.bgTextData.length)]);
     }
+
+    this.particleStyle = {
+      position: 'fixed',
+      width: '100%',
+      height: '100%',
+      'z-index': -1,
+      top: 0,
+      left: 0,
+      right: 0,
+      bottom: 0,
+    };
+
+    this.particleParams = {
+      particles: {
+        number: {
+          value: 80,
+          density: {
+            enable: true,
+            value_area: 800
+          }
+        },
+        color: {
+          value: '#ffffff'
+        },
+        shape: {
+          type: 'polygon',
+          polygon: {
+            nb_sides: 5
+          },
+        },
+        opacity: {
+          value: 0.5,
+          random: true,
+        },
+        size: {
+          value: 3,
+          random: true,
+        },
+        line_linked: {
+          enable: false,
+        },
+        move: {
+          enable: true,
+          speed: 3,
+          direction: 'top',
+          random: true,
+          straight: false,
+          out_mode: 'out',
+        }
+      },
+      retina_detect: true
+    };
   }
 
 }
